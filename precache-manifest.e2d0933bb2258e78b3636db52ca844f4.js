4d348f1430e8bf95bc55066bb5cd9fe007bebcfd
belaforreiter <belaforreiter@Belas-MacBook-Pro.local>
2019-08-12 10:35:36 +0300

    Updates

---
+++ precache-manifest.e2d0933bb2258e78b3636db52ca844f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c53aacaad6fe8f44fa3f6d1b547c0b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8be5c0a284f6a24cf78",
+    "url": "/monsters-rolodex/static/css/main.10a44e76.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b8be5c0a284f6a24cf78",
+    "url": "/monsters-rolodex/static/js/main.1402c175.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
